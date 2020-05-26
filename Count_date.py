d0fa845343322aaed63c1dc8823391caeb878e5d
wujian <863473277@qq.com>
2020-05-26 17:43:22 +0800

    add:#7 产品类型产生

--- Count_date.py
+++ Count_date.py
@@ -7,9 +7,10 @@ def opt_date(number):
     if isinstance(number, int):
         cut_date = new_time + datetime.timedelta(days=number)
         old_date_str = datetime.datetime.strftime(cut_date, "%Y-%m-%d")
-        print(old_date_str)
-        #print("修改信息")
+        # print(old_date_str)
+        new_time = "这是今天的时间：{}".format(new_time)
+        print(new_time)
         return old_date_str
 
 if __name__ == '__main__':
-    opt_date(-1)
+    opt_date(1)
