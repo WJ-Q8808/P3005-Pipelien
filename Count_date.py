b202f929ff209c5ed76dcea0ea358547a5b6cae0
WJ-Q8808 <863473277@qq.com>
2020-06-17 17:29:08 +0800

    修改缺陷

--- Count_date.py
+++ Count_date.py
@@ -8,16 +8,16 @@ def opt_date(number):
         cut_date = new_time + datetime.timedelta(days=number)
         old_date_str = datetime.datetime.strftime(cut_date, "%Y-%m-%d")
         # print(old_date_str)
-        new_time = "这是今天的时间：{}".format(new_time)
-        print(new_time)
+        Nw_time = "这是今天的时间：{}".format(new_time)
+        print(Nw_time)
         return old_date_str
 
- def SetType(datatype):
+def set_type(datatype):
      Exeamplelist = datatype
      SetList = list(set(Exeamplelist))
      return SetList
 
 if __name__ == '__main__':
     type_list = ["bc","bc","权限","leixing"]
-    print(SetType(type_list))
+    print(set_type(type_list))
     print(opt_date(-1))
