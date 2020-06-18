51db9d832dad2a1abab38c99b3372d8ca5481f24
WJ-Q8808 <863473277@qq.com>
2020-06-18 15:47:08 +0800

    add:hot

--- SonarQube/hot/hotspotsecure.py
+++ SonarQube/hot/hotspotsecure.py
@@ -32,3 +32,22 @@ import os
 def send_signal(pid, sig, pgid):
     os.kill(pid, sig)  # Sensitive
     os.killpg(pgid, sig)  # Sensitive
+
+
+
+
+import os
+
+value = input()
+command = 'os.system("%s")' % value
+
+def evaluate(command, file, mode):
+    eval(command)  # Sensitive.
+
+eval(command)  # Sensitive. Dynamic code
+
+def execute(code, file, mode):
+    exec(code)  # Sensitive.
+    exec(compile(code, file, mode))  # Sensitive.
+
+exec(command)  # Sensitive.
