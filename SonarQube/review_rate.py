798e472b95e5b5fef3d22274c207795ebcf32d5a
WJ-Q8808 <863473277@qq.com>
2020-06-20 10:01:00 +0800

    update:覆盖率

--- SonarQube/review_rate.py
+++ SonarQube/review_rate.py
@@ -2,3 +2,37 @@
 #-------------------------------------------------------------------------------
 review_rate = "复审率"
 
+# -*- coding: utf-8 -*-#
+
+repeat_block = "{}:代码块".format("重复率")
+
+def count_number(start):
+    add_number = 0
+    add_number += int(start)
+    print(count_number)
+    return count_number
+
+def count_str(start):
+    add_number = ""
+    add_number += start
+    print(count_str)
+    return count_str
+
+def count_list(start):
+    add_number = ["list01","list02"]
+    add_number.append(start)
+    count_list = len(add_number)
+    print(count_list)
+    print(count_list)
+    print(count_list)
+    print(count_list)
+    print(count_list)
+    print(count_list)
+    print(count_list)
+    return count_list
+
+if __name__ == '__main__':
+    count_number(2)
+    count_str("str1")
+    count_list("ces")
+
--- test/test.py
+++ test/test.py
@@ -1,7 +1,21 @@
 # -*- coding: utf-8 -*-# 
 #-------------------------------------------------------------------------------
-# Name:         test.py
-# Description:  
-# Author:       wujian
-# Date:         2020/6/18
-#-------------------------------------------------------------------------------
+import unittest
+from SonarQube import review_rate
+
+
+class TestStringMethods(unittest.TestCase):
+
+
+
+    def test_case_01(self):
+        count_number = review_rate.count_number(2)
+        self.assertEqual(count_number, 2)
+
+    def test_case_02(self):
+        count_str = review_rate.count_str("test")
+        self.assertEqual(count_str,"test")
+
+if __name__ == '__main__':
+    unittest.main()
+
